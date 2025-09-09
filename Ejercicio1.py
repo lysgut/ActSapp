2696bdd3058d61d2dee95563687402591249a32b
santigiamba <sgiambartolomeichavarria@escuelasproa.edu.ar>
2025-09-09 09:45:09 -0300

    terminado

---
+++ Ejercicio1.py
@@ -0,0 +1,15 @@
+edad = 18
+nombre = "Lucas"
+tiene_lnc = True
+
+if edad >= 18:
+    print(nombre, "es mayor de edad.")
+
+
+if edad >= 18:
+    if tiene_lcn:
+        print(nombre, "puede manejar porque es mayor y tiene licencia nacional de conducir.")
+    else:
+        print(nombre, "es mayor de edad pero no tiene licencia nacional de conducir.")
+else:
+    print(nombre, "es menor de edad.")
